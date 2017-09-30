cf3941c420601b2d118b08b27ce84256d150f5ef
takahashik <takahashik@global-solutions.co.jp>
2017-09-30 15:19:15 +0900

    storybookでreact-toolboxを表示できるようにする
    * webpack.config でstyle-loader, css-loader, postcss-loaderを使うようにする
      * postcss-loaderの設定ファイルがないとエラーになるので、空のpostcss.config.js を追加

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,14 +5,30 @@
 // IMPORTANT
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    loaders: [
-      // add your custom loaders.
-    ],
-  },
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+  const cssLoader = config.module.rules.find(l => l.test.test('.css'));
+  cssLoader.use = [
+    'style-loader',
+    {
+      loader: 'css-loader',
+      options: {
+        modules: true, // default is false
+        sourceMap: true,
+        importLoaders: 1,
+        localIdentName: '[name]__[local]___[hash:base64:5]',
+      },
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        config: {
+          path: '.storybook/postcss.config.js',
+        },
+      },
+    },
+  ];
+  return config;
 };
