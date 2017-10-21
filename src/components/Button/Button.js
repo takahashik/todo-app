3c81b9730756b4923786774fdf0b4f44176e9a77
takahashik <takahashik@global-solutions.co.jp>
2017-10-21 18:17:11 +0900

    flow typeを修正
    * typeを任意にする
    * exact object のスプレッド演算がエラーになるので対応

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,19 +1,20 @@
 // @flow
 
 import React from 'react';
+import type { Element } from 'react';
 import { Button as RTButton } from 'react-toolbox/lib/button';
 import RTButtonTheme from 'react-toolbox/lib/button/theme.css';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 /* eslint-disable react/require-default-props */
-export type ButtonPropType = {|
+export type T = {
   accent?: boolean,
   className?: string,
   disabled?: boolean,
   flat?: boolean,
   floating?: boolean,
   href?: string,
-  icon?: string | React.Element<any>,
+  icon?: string | Element<*>,
   inverse?: boolean,
   label?: string,
   mini?: boolean,
@@ -37,9 +38,10 @@ export type ButtonPropType = {|
     ripple?: string,
     toggle?: string,
   |},
-  type: string,
-|};
+  type?: string,
+};
 /* eslint-eable react/require-default-props */
+type ButtonPropType = T & $Shape<T>; // eslint-disable-line no-undef
 
 function Button(props: ButtonPropType) {
   return (
