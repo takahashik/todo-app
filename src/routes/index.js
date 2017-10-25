af125aa51a3b5a87ea22b8eb6526d15d01021b00
takahashik <takahashik@global-solutions.co.jp>
2017-10-25 21:31:26 +0900

    feat(routes): Todoページを追加
    * 未完成なので工事中で追加する

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,7 +23,7 @@ export default {
     require('./about').default,
     require('./privacy').default,
     require('./admin').default,
-
+    require('./todo').default,
     // Wildcard routes, e.g. { path: '*', ... } (must go last)
     require('./notFound').default,
   ],
